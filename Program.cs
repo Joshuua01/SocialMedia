a78da1e0de1d6efd5ccdaaac1344cdc65bcfb4d8
Jan Steinbichler <jsteinbichler@x-formation.com>
2023-02-08 16:37:53 +0100

    Add Razor Maping and Login Partial View

--- Program.cs
+++ Program.cs
@@ -41,6 +41,8 @@ namespace SocialMedia
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
 
+            app.MapRazorPages();
+
             app.Run();
         }
     }
