fd48436356acfb494af9d75f2091823a1f7f4c14
Jan Steinbichler <jsteinbichler@x-formation.com>
2023-02-08 22:52:07 +0100

    Seed Data

--- Areas/Identity/Data/ApplicationDbContext.cs
+++ Areas/Identity/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using SocialMedia.Areas.Identity.Data;
 using SocialMedia.Models;
+using System.Reflection.Emit;
 
 namespace SocialMedia.Data;
 
@@ -19,26 +20,69 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
+        //Maping Enum to String
         base.OnModelCreating(builder);
         builder.Entity<Reaction>()
             .Property(r => r.reactionType)
             .HasConversion<string>();
 
-        /* builder.Entity<IdentityRole>().HasData(
-             new IdentityRole
-             {
-                 Id = "1",
-                 Name = "Admin",
-                 NormalizedName = "ADMIN"
-             },
-             new IdentityRole
-             {
-                 Id = "2",
-                 Name = "User",
-                 NormalizedName = "USER"
-             }
-         );*/
+        //Seed Data
 
-
+        //Seed Roles
+        builder.Entity<IdentityRole>().HasData(
+            new IdentityRole
+            {
+                Id = "2c5e174e-3b0e-446f-86af-483d56fd7210",
+                Name = "Admin",
+                NormalizedName = "ADMIN"
+            },
+            new IdentityRole
+            {
+                Id = "2c5e174e-3b0e-446f-86af-483d56fd7211",
+                Name = "User",
+                NormalizedName = "USER"
+            }
+        );
+        //Seed Admin and User
+        var hasher = new PasswordHasher<ApplicationUser>();
+        builder.Entity<ApplicationUser>().HasData(
+            new ApplicationUser
+            {
+                Id = "8e445865-a24d-4543-a6c6-9443d048cdb9",
+                UserName = "admin@admin.admin",
+                NormalizedUserName = "ADMIN@ADMIN.ADMIN",
+                PasswordHash = hasher.HashPassword(null, "admin"),
+                Email = "admin@admin.admin",
+                NormalizedEmail = "ADMIN@ADMIN.ADMIN",
+                PhoneNumber = "123456789",
+                FirstName = "Admin",
+                LastName = "Admin"
+            },
+            new ApplicationUser
+            {
+                Id = "8e445865-a24d-4543-a6c6-9443d048cdb8",
+                UserName = "user@user.user",
+                NormalizedUserName = "USER@USER.USER",
+                PasswordHash = hasher.HashPassword(null, "user"),
+                Email = "user@user.user",
+                NormalizedEmail = "USER@USER.USER",
+                PhoneNumber = "987654321",
+                FirstName = "User",
+                LastName = "User"
+            }
+         );
+        //Add roles for Admin and User
+        builder.Entity<IdentityUserRole<string>>().HasData(
+            new IdentityUserRole<string>
+            {
+                RoleId = "2c5e174e-3b0e-446f-86af-483d56fd7210",
+                UserId = "8e445865-a24d-4543-a6c6-9443d048cdb9"
+            },
+            new IdentityUserRole<string>
+            {
+                RoleId = "2c5e174e-3b0e-446f-86af-483d56fd7211",
+                UserId = "8e445865-a24d-4543-a6c6-9443d048cdb8"
+            }
+        );
     }
 }
---
+++ Migrations/20230208215043_SeedData.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SocialMedia.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "2c5e174e-3b0e-446f-86af-483d56fd7210", "7a5f80be-055e-43b3-923a-480cd6b79bef", "Admin", "ADMIN" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "2c5e174e-3b0e-446f-86af-483d56fd7211", "cd0105f1-037d-402f-aa8c-fae4440a6558", "User", "USER" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "ConcurrencyStamp", "CreationDate", "Email", "EmailConfirmed", "FirstName", "LastName", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[] { "8e445865-a24d-4543-a6c6-9443d048cdb8", 0, "87b6f587-ee3d-47dd-9001-01388a8cfe7e", new DateTime(2023, 2, 8, 22, 50, 42, 717, DateTimeKind.Local).AddTicks(5058), "user@user.user", false, "User", "User", false, null, "USER@USER.USER", "USER@USER.USER", "AQAAAAEAACcQAAAAEE28HXmNkUlVP0Hbr1plLtgDXA0R3h8zq4opPzkVtak/wl7jEQM634iZ7B0mncynnA==", "987654321", false, "7489910e-baf7-4d79-819f-315cd2796fb5", false, "user@user.user" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "ConcurrencyStamp", "CreationDate", "Email", "EmailConfirmed", "FirstName", "LastName", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[] { "8e445865-a24d-4543-a6c6-9443d048cdb9", 0, "b6de8978-4ffe-4059-b9d8-3c2086ea1c69", new DateTime(2023, 2, 8, 22, 50, 42, 709, DateTimeKind.Local).AddTicks(8026), "admin@admin.admin", false, "Admin", "Admin", false, null, "ADMIN@ADMIN.ADMIN", "ADMIN@ADMIN.ADMIN", "AQAAAAEAACcQAAAAEFXmou7t2i/MjkLR1JX/kC8iLtmmo7qIOjx72X06YkVQRzMQAB3kkX9tgYd41CyGNw==", "123456789", false, "aa640e38-7123-434d-8aac-dcc10d24d304", false, "admin@admin.admin" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUserRoles",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[] { "2c5e174e-3b0e-446f-86af-483d56fd7211", "8e445865-a24d-4543-a6c6-9443d048cdb8" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUserRoles",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[] { "2c5e174e-3b0e-446f-86af-483d56fd7210", "8e445865-a24d-4543-a6c6-9443d048cdb9" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { "2c5e174e-3b0e-446f-86af-483d56fd7211", "8e445865-a24d-4543-a6c6-9443d048cdb8" });
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { "2c5e174e-3b0e-446f-86af-483d56fd7210", "8e445865-a24d-4543-a6c6-9443d048cdb9" });
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "2c5e174e-3b0e-446f-86af-483d56fd7210");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "2c5e174e-3b0e-446f-86af-483d56fd7211");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "8e445865-a24d-4543-a6c6-9443d048cdb8");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "8e445865-a24d-4543-a6c6-9443d048cdb9");
+        }
+    }
+}
